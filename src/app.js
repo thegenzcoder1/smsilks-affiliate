8f766f5e13091b64f1e70d3ec5f69c73ca22e396
Hariharan253 <hari.haran.210325@gmail.com>
2026-02-07 18:24:07 +0530

    updated postman access_logic

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
      CASE 2: Server-to-server
      (nginx / curl / healthcheck)
   ========================== */
-  if (!origin && secret_access_key === process.env.POSTMAN_ACCESS_KEY) {
+  if (!origin && (secret_access_key === process.env.POSTMAN_ACCESS_KEY)) {
     isAllowed = true;
   }
 
